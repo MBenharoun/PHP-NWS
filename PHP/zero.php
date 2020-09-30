f21d7127670ec8b8484f6fb6afb929bdd44c7320
Marwane Benharoun <71384548+MBenharoun@users.noreply.github.com>
2020-09-30 08:55:08 +0200

    Delete zero.php

--- PHP/zero.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function LettreDelete($chaine , $tableau)
-{
-    $replace = '';
-    print_r(str_replace($tableau,$replace,$chaine));
-}
-LettreDelete('Suprey91mol',array('e','r','y','o'));
